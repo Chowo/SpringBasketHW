853178a43c15a4a34f15380819c06121244bc762
Chowo <bob-xiii@rambler.ru>
2024-08-01 05:37:51 +0200

    Initial commit

---
+++ src/main/java/pro/sky/spring_home_work_basket1/SpringHomeWorkBasket1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.spring_home_work_basket1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeWorkBasket1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeWorkBasket1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/spring_home_work_basket1/SpringHomeWorkBasket1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.spring_home_work_basket1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeWorkBasket1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
